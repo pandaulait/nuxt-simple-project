8f0ba71bcae3a683b35740f9e915f7c7b74a3db9
森山啓輔 <>
2022-01-06 10:54:55 +0900

    remove tag embedding.

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,6 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    '@nuxtjs/dotenv',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,9 @@
 <template>
   <div>
-    <ul>
-      <li v-for="(content, index) in contents" :key="index">
-        <nuxt-link :to="`/${content.public_uid}`">{{
-          content.fields.title.value
-        }}</nuxt-link>
-      </li>
-    </ul>
+    HelloWorld!
   </div>
 </template>
 <script>
 export default {
-
-  async asyncData({ $axios }) {
-    const response = await $axios.get(
-      `https://cms.spearly.app/api/v1/content_types/${process.env.CONTENT_TYPE_ID}/contents`,
-      {
-        headers: {
-          Authorization:
-            `Authorization: Bearer ${process.env.API_KEY}`
-        }
-      }
-    );
-    return { contents: response.data.contents };
-  }
 };
 </script>
